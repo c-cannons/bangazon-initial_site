f0db436d07ac59492395310a3af15df32b2cf262
Michael Lindstrom <michaellindstr@gmail.com>
2017-11-14 14:26:32 -0600

    adding seed data

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -1,2 +1,14 @@
 class ProductTypesController < ApplicationController
+
+    # This is only used for the faker gem
+    def create
+        @product_type = ProductType.new(product_type_params)
+        @product_type.save
+    end
+
+    private
+        def product_type_params
+            params.require(:product_type).permit(:product_type_name)
+        end
+
 end
