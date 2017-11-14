4a2610ac6da649593bd9cf186fcff5661cdf75c7
daniel-certa-1228 <mfdancerta@gmail.com>
2017-11-14 11:36:42 -0600

    added and modified auth boilerplate

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,21 @@
 class CustomersController < ApplicationController
+    def new
+        @customer = Customer.new
+    end
+    
+    def create
+        @customer = Customer.new(customer_params)
+        if @customer.save
+            session[:customer_id] = @customer.id
+            redirect_to root_url, notice: 'Thank you for signing up!'
+        else
+            render :new
+        end
+    end
+
+    private
+    
+        def customer_params
+            params.require(:customer).permit(:email, :password, :password_confirmation)
+        end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    def new
+    end
+  
+    def create
+      customer = Custmomer.find_by(email: params[:email])
+      if customer && customer.authenticate(params[:password])
+        session[:customer_id] = customer.id
+        redirect_to root_url, notice: 'Logged in!'
+      else
+        flash.now.alert = 'Email or password is invalid'
+        render :new
+      end
+    end
+  
+    def destroy
+      session[:customer_id] = nil
+      redirect_to root_url, notice: 'Logged out!'
+    end
 end
