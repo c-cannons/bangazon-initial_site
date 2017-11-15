2a94022e3af4807d44edae5c1faaab46959243bd
Michael Lindstrom <michaellindstr@gmail.com>
2017-11-15 09:32:44 -0600

    working on viewing all product types

--- db/migrate/20171114155853_create_products.rb
+++ db/migrate/20171114155853_create_products.rb
@@ -6,6 +6,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.string :product_name, null:false
       t.decimal :product_price, precision: 6, scale: 2, null:false
       t.string :product_desc, null:false
+      t.integer :quantity
       t.string :product_location
       t.attachment :product_picture
       t.timestamps
