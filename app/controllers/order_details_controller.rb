763838e742f7a70178ba8d41ebe4d946703d094a
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-11-16 14:09:14 -0600

    cart functions working, delete working

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -1,12 +1,21 @@
 class OrderDetailsController < ApplicationController
 
     def index
-        @order_details = OrderDetail.all
+        @buyer_id = session[:customer_id]
+        # @shopping_cart = Product.joins(:orders).where("orders.customer_id = #{@buyer_id}")
+        @shopping_cart = Product.joins(:orders).where("orders.customer_id = #{@buyer_id}").select('order_details.id AS order_details_id, products.product_name, products.product_price, products.product_desc, products.product_location')
     end
 
     def create
         @order_detail = OrderDetail.new(order_detail_params)
         @order_detail.save
+    end
+
+    def destroy
+        @order_detail = OrderDetail.find(params[:id])
+        @order_detail.destroy
+
+        redirect_to order_details_path
     end
 
     private
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,14 @@
 class OrdersController < ApplicationController
 
     def index
-        # @orders = Order.where("customer_id = '1' AND payment_method_id = '0'" )
-        @shopping_cart = Product.joins(:orders).where("orders.customer_id = '1'")
     end
 
     def create
         @order = Order.new(order_params)
         @order.save
+    end
+
+    def destroy
     end
 
     private
