c5bf4b5e7d45fc5c5ded4fe41e1c2d33227c897a
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-11-17 13:21:53 -0600

    shopping cart add, delete, and clear cart functions working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,31 @@ class SessionsController < ApplicationController
 
     def new
     end
-  
+
     def create
       customer = Customer.find_by(email: params[:email])
       if customer && customer.authenticate(params[:password])
         session[:customer_id] = customer.id
+
+        order = Order.find_by(customer_id: customer.id, payment_method: nil)
+        if order
+          session[:order_id] = order.id
+        else
+          @new_order = Order.create!(:customer_id => customer.id, :payment_method_id => nil)
+          # new_order.customer_id = session[:customer_id]
+          # new_order.payment_method_id = nil
+          # @new_order.save
+          # order = Order.find_by(customer_id: customer.id, payment_method: nil)
+          session[:order_id] = 'No Order'
+        end
+        # can set order id here
         redirect_to root_url, notice: 'Thank you for logging in!'
       else
         flash.now.notice = 'Email or password is invalid'
         render :new
       end
     end
-  
+
     def destroy
       session[:customer_id] = nil
       redirect_to root_url, notice: 'Logged out! Buy something next time.'
--- db/migrate/20171114155923_create_orders.rb
+++ db/migrate/20171114155923_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
       t.references :customer, index:true, foreign_key:true, null:false
-      t.references :payment_method, index:true, foreign_key:true, null:false
+      t.references :payment_method, index:true, foreign_key:true
       t.timestamps
     end
   end
