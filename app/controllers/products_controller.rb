040aee2eff809786859098b46bc6f9c6dec84755
daniel-certa-1228 <mfdancerta@gmail.com>
2017-11-20 14:59:24 -0600

    profanity filter working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,11 +20,23 @@ class ProductsController < ApplicationController
     end
 
     def create
+        regex = curses
         @product = Product.new(product_params)
-        if @product.save
-            redirect_to @product
-        else
+        if @product.product_name.match(regex)&&@product.product_desc.match(regex)
+            flash.now[:notice] = "Your product name and description must be free of profanity." 
             render 'new'
+        elsif @product.product_name.match(regex)
+            flash.now[:notice] = "Your product name must be free of profanity." 
+            render 'new'
+        elsif @product.product_desc.match(regex)
+            flash.now[:notice] = "Your product description must be free of profanity." 
+            render 'new'
+        else
+            if @product.save
+                redirect_to @product
+            else
+                render 'new'
+            end
         end
     end
 
@@ -40,6 +52,7 @@ class ProductsController < ApplicationController
     end
 
     private
+
         def product_params
             params.require(:product).permit(:customer_id, :product_type_id, :product_name, :product_price, :product_desc, :quantity, :product_location, :avatar)
         end
@@ -47,5 +60,10 @@ class ProductsController < ApplicationController
         def cannot_delete
             redirect_to products_path
             flash[:notice] = 'This product is part of an order and cannot be deleted.'
+        end
+
+        def curses
+            regexArray = [/([Ss]hit(, )?)+/,/([Pp]iss(, )?)+/, /([Ff]uck(, )?)+/, /([Cc]unt(, )?)+/, /([Cc]ocksucker(, )?)+/, /([Mm]otherfucker(, )?)+/, /([Tt]its(, )?)+/, /([Aa]ss(, )?)+/, /([Tt]wat(, )?)+/]
+            return Regexp.union(regexArray)
         end
 end
